acdf5eec63b67c72820f57da570b3e262b783d26
Paulina Adams <pstafska@gmail.com>
2024-11-12 10:53:39 +0000

    Modify:index function in hello_world/views.py

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,5 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Hello World</h1>")
+
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
 
